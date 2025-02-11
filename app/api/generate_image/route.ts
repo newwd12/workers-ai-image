6441075d015aa2c346c62d7731651acc953f1e26
newwd12 <wangdan@holyzone.com.cn>
2025-02-11 09:59:10 +0800

    Update route.ts

--- app/api/generate_image/route.ts
+++ app/api/generate_image/route.ts
@@ -9,7 +9,8 @@ export const runtime = 'edge'
 export async function POST(request: NextRequest) {
   try {
     const context = getRequestContext()
-    const { AI, BUCKET } = context.env
+    //const {AI, BUCKET} = context.env
+    const {AI} = context.env
     let { prompt, model } = await request.json<{ prompt: string, model: string }>()
     if (!model) model = "@cf/black-forest-labs/flux-1-schnell"
 
@@ -20,8 +21,8 @@ export async function POST(request: NextRequest) {
     const binaryString = atob(response.image);
 
     // @ts-ignore
-    const img = Uint8Array.from(binaryString, (m) => m.codePointAt(0));
-    await BUCKET.put(`${promptKey}.jpeg`, img)
+    //const img = Uint8Array.from(binaryString, (m) => m.codePointAt(0));
+    //await BUCKET.put(`${promptKey}.jpeg`, img)
 
     return new Response(`data:image/jpeg;base64,${response.image}`, {
       headers: {
